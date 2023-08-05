8454d52e898bb178e640ec8cfba89e2641f5d772
frayfray248 <nicman248@hotmail.com>
2023-08-05 14:50:12 -0700

    planet fetching

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,28 @@
+// modules
 import StyleWrapper from "./styles/StyleWrapper"
+
+// components
+import Main from "../components/Main"
+
+async function getData() {
+    const res = await fetch('http://localhost:3000/api/getPlanets',
+        {
+            method: 'GET',
+            redirect: 'follow',
+            next: {
+                revalidate: 3600
+            }
+        })
+
+    if (!res.ok) {
+
+        throw new Error('Failed to fetch data')
+    }
+
+    const data = await res.json()
+
+    return data
+}
 
 export default async function Page() {
 
---
+++ src/pages/api/getPlanets.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+async function getPlanets() {
+
+    const planets = await axios.get('https://exoplanetarchive.ipac.caltech.edu/TAP/sync/?query=select+pl_name,disc_year+from+ps&format=json',
+        {
+            method: 'GET',
+            redirect: 'follow',
+        })
+
+    return planets.data
+}
+
+export default async function (req, res) {
+
+    
+
+    const planets = await getPlanets()
+    res.status(200).json(planets)
+
+}
